23669fb0ff1a17fa69a5a61c12cbe0f6ba4d8403
chstrand <35688226+chstrand@users.noreply.github.com>
2020-09-17 12:05:34 +0200

    Add files via upload

--- Power_calc.py
+++ Power_calc.py
@@ -2,12 +2,29 @@ from math import*
 
 #Variables
 
-tourqe = 10.5
-rpm = 9000
+def rad2degree(rad):
+	degree = rad*180/pi
+	return degree
+	
+
+def degree2rad(degree):
+	rad = degree*pi/180
+	return rad
+
+#tourqe = 10.5
+#rpm = 9000
 r_platform = 0.85
-mass = 200
+mass = 300
 angle = 45
 r_pully = 0.05
+
+degree_acc = 200									#degree/sec^2
+
+Motor_power = 3000									#power of motor (W)	
+
+ang_acc = degree2rad(degree_acc)							#rad/sec^2
+
+lin_acc = ang_acc*r_platform
 
 #Calc 1 
 
@@ -15,38 +32,31 @@ s = 2*pi*r_platform*angle/360 				#Arc length of rail
 
 varv = s/(r_pully*2*pi)						#Revolutions of pully
 
-#print("varv =", varv)
+print("revs required =", varv)
 
-rad = varv*2*pi 							#Total radians
+#rad = varv*2*pi 							#Total radians
 
 #print("rad =", rad)
 
 #Calc 2
 
-Tourqe_person = mass*9.82*r_pully			#Torque of person
+F_req = mass*lin_acc + mass*9.82
 
-Power_motor_chosen = tourqe*2*pi*rpm/60		#Torque of chosen motor
+tourqe_req = F_req*r_pully
 
-Power_motor_needed = Tourqe_person*rad 		#Torque needed
+print("Force required =",F_req)
 
-Power_factor = Power_motor_chosen/Power_motor_needed 		#Torque factor
+print("torque required =",tourqe_req)
 
+omega = Motor_power/tourqe_req
 
-print(" Power chosen =", Power_motor_chosen)
-print(" Power needed =", Power_motor_needed)
-print(" Factor =",Power_factor)
+#print(omega)
 
+varv_sec = omega/(2*pi)
 
-#Calc 3
+print("revs achived =",varv_sec)
 
-ang_vel = Power_motor_chosen/Tourqe_person					#Rad per sec
+factor = varv_sec/varv
 
-RPS = ang_vel/(2*pi)										#Revs per sec
-
-possible_gear_ratio = RPS/varv
-
-
-print(" RPM =",RPS)
-
-print(" Possible gear ratio =",possible_gear_ratio)
+print("gear ratio possible =",factor)
 
